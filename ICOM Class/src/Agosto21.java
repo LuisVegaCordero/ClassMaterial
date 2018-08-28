98f5eafb24453c619cd14ef22b6fc1637bc928a7
Luis <luis.vega21@upr.edu>
2018-08-27 21:46:16 -0400

    Initial Commit of past class material.

---
+++ ICOM Class/src/Agosto21.java
@@ -0,0 +1,41 @@
+import java.lang.Math;
+
+public class Agosto21 {
+
+	
+	public static void main(String[] args) {
+		
+		Triangle ourTriangle = new Triangle(10,10,10);
+		double area = ourTriangle.areaofTriangle();
+		System.out.println("The area of the triangle is " +area);
+		
+		Triangle ourTriangle2 = new Triangle(20,20,20);
+		area = ourTriangle2.areaofTriangle();
+		System.out.println("The area of the second triangle is " +area);
+		
+//		Triangle theTriangle1 = new Triangle(10.0, 10.0, 10.0);
+//		double area = theTriangle1.areaofTriangle();
+				
+		// Last Class
+		
+		
+//		double side1 = 10.0;
+//		double side2 = 10.0;
+//		double side3 = 10.0;
+//		
+//		double area = Functions.areaOfTriangle(side1, side2, side3);
+//		
+//		System.out.println("The area of your triangle is " +area);
+//		
+//		side1 = 10.0;
+//		side2 = 10.0;
+//		side3 = 10.0;
+//		
+//		area = Functions.areaOfTriangle(side1, side2, side3);
+//		
+		
+		
+
+		
+	}
+}
